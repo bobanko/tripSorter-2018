21594a2d7aa02a0748b5ff7232a13cbc4cd73432
bobanko <mishko.vladimir@gmail.com>
2018-11-12 19:34:34 +0200

    webpack build configured

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "[name].[hash].js"
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 12000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      { enforce: "pre", test: /\.tsx?$/, loader: "source-map-loader" }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist/*"]),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
+  ],
+
+  devServer: {
+    //host: 'localhost', //default
+    //port: 8080, //default
+    //port: 9000
+    contentBase: "./dist"
+    //compress: true,
+    //hot: true
+  }
+};
