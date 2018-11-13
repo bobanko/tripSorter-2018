fa67d91ab94c76e6b70df399d8564f8df35ea845
bobanko <mishko.vladimir@gmail.com>
2018-11-13 20:43:16 +0200

    base url extracted to const

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -13,6 +13,8 @@ import { SearchResults } from "./search-results";
 
 import "./index.scss";
 
+const baseUrl = "/tripSorter-2018/dist";
+
 const Header = () => (
   <ul>
     <li>
@@ -32,7 +34,7 @@ class App extends React.Component {
     return (
       <div className="app flex--col flex--center">
         <h1 className="flex--row flex--center">TripSorter</h1>
-        <Router basename="/tripSorter-2018/dist">
+        <Router basename={baseUrl}>
           <Switch>
             <Route path="/search" component={SearchForm} />
             <Route path="/results" component={SearchResults} />
