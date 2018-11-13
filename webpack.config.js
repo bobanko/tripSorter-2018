c9b04f131cfd1f283fb83703e2836e4181deff5b
bobanko <mishko.vladimir@gmail.com>
2018-11-13 04:35:25 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: "./src/index.jsx",
   output: {
     filename: "[name].[hash].js",
-    publicPath: "/"
+    publicPath: "/dist"
   },
   watchOptions: {
     ignored: /node_modules/
